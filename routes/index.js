e8b34a40f45e7d5ef7db653ba7e9a6e76d2940ba
alikharrat <kharratali@hotmail.com>
2018-03-29 23:15:27 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-const dialogflow = require('dialogflow');
 /* GET home page. */
 createEntityTypes("heroic-artifact-160907");
 router.get('/', function(req, res, next) {
@@ -22,81 +21,6 @@ router.post('/Send_Message',function(req,res){
     source: "webhook-echo-sample"
   });
 })
-
-// /////////////////////////////////////////////////////////////////////////////
-// Operations for entity types.
-// /////////////////////////////////////////////////////////////////////////////
-
-function createEntityTypes(projectId) {
-  // [START dialogflow_create_entity]
-  // Imports the Dialogflow library
-  // Instantiates clients
-  const entityTypesClient = new dialogflow.EntityTypesClient();
-  const intentsClient = new dialogflow.IntentsClient();
-
-  // The path to the agent the created entity type belongs to.
-  const agentPath = intentsClient.projectAgentPath(projectId);
-
-  const promises = [];
-
-  // Create an entity type named "size", with possible values of small, medium
-  // and large and some synonyms.
-  const sizeRequest = {
-    parent: agentPath,
-    entityType: {
-      displayName: 'size',
-      kind: 'KIND_MAP',
-      autoExpansionMode: 'AUTO_EXPANSION_MODE_UNSPECIFIED',
-      entities: [
-        {value: 'small', synonyms: ['small', 'petit']},
-        {value: 'medium', synonyms: ['medium']},
-        {value: 'large', synonyms: ['large', 'big']},
-      ],
-    },
-  };
-  promises.push(
-    entityTypesClient
-      .createEntityType(sizeRequest)
-      .then(responses => {
-        console.log('Created size entity type:');
-        logEntityType(responses[0]);
-      })
-      .catch(err => {
-        console.error('Failed to create size entity type:', err);
-      })
-  );
-
-  // Create an entity of type named "topping", with possible values without
-  // synonyms.
-  const toppingRequest = {
-    parent: agentPath,
-    entityType: {
-      displayName: 'topping',
-      kind: 'KIND_LIST',
-      autoExpansionMode: 'AUTO_EXPANSION_MODE_UNSPECIFIED',
-      entities: [
-        {value: 'tomato', synonyms: ['tomato']},
-        {value: 'tuna', synonyms: ['tuna']},
-        {value: 'cheddar', synonyms: ['cheddar']},
-        {value: 'mushrooms', synonyms: ['mushrooms']},
-      ],
-    },
-  };
-  promises.push(
-    entityTypesClient
-      .createEntityType(toppingRequest)
-      .then(responses => {
-        console.log('Created topping entity type:');
-        logEntityType(responses[0]);
-      })
-      .catch(err => {
-        console.error('Failed to create topping entity type:', err);
-      })
-  );
-
-  return Promise.all(promises);
-  // [END dialogflow_create_entity]
-}
 
 
 module.exports = router;
