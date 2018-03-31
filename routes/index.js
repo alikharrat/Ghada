875026697dbb398a1c3ff830dd509e58f3997d2a
alikharrat <kharratali@hotmail.com>
2018-03-31 13:45:13 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const dialogflow = require('dialogflow');
 /* GET home page. */
+createEntityTypes("heroic-artifact-160907");
 router.get('/', function(req, res, next) {
   console.log(req.body)
   res.render('index', { title: 'Express' });
@@ -21,6 +23,84 @@ router.post('/Send_Message',function(req,res){
   });
 })
 
+// /////////////////////////////////////////////////////////////////////////////
+// Operations for entity types.
+// /////////////////////////////////////////////////////////////////////////////
+
+function createEntityTypes(projectId) {
+  // [START dialogflow_create_entity]
+  // Imports the Dialogflow library
+  // Instantiates clients
+  console.log('je suis là')
+  const entityTypesClient = new dialogflow.EntityTypesClient();
+  const intentsClient = new dialogflow.IntentsClient();
+
+  // The path to the agent the created entity type belongs to.
+  const agentPath = intentsClient.projectAgentPath(projectId);
+
+  const promises = [];
+
+  // Create an entity type named "size", with possible values of small, medium
+  // and large and some synonyms.
+  const sizeRequest = {
+    parent: agentPath,
+    entityType: {
+      displayName: 'size2',
+      kind: 'KIND_MAP',
+      autoExpansionMode: 'AUTO_EXPANSION_MODE_UNSPECIFIED',
+      entities: [
+        {value: 'small', synonyms: ['small', 'petit']},
+        {value: 'medium', synonyms: ['medium']},
+        {value: 'large', synonyms: ['large', 'big']},
+      ],
+    },
+  };
+  promises.push(
+    entityTypesClient
+      .createEntityType(sizeRequest)
+      .then(responses => {
+        console.log('Created size entity type:');
+        logEntityType(responses[0]);
+      })
+      .catch(err => {
+        console.error('Failed to create size entity type:', err);
+      })
+  );
+
+function logEntityType(entityType) {
+  // Imports the Dialogflow library
+  const dialogflow = require('dialogflow');
+
+  // Instantiates client.
+  const entityTypesClient = new dialogflow.EntityTypesClient();
+
+  console.log(
+    '  ID:',
+    entityTypesClient.matchEntityTypeFromEntityTypeName(entityType.name)
+  );
+  console.log('  Display Name:', entityType.displayName);
+  console.log(
+    '  Auto expansion:',
+    entityType.autoExpansionMode === 'AUTO_EXPANSION_MODE_DEFAULT'
+  );
+  if (!entityType.entities) {
+    console.log('  No entity defined.');
+  } else {
+    console.log('  Entities: ');
+    entityType.entities.forEach(entity => {
+      if (entityType.kind === 'KIND_MAP') {
+        console.log(`    ${entity.value}: ${entity.synonyms.join(', ')}`);
+      } else {
+        console.log(`    ${entity.value}`);
+      }
+    });
+  }
+  console.log('');
+}
+
+  return Promise.all(promises);
+  // [END dialogflow_create_entity]
+}
+
 
 module.exports = router;
-
